64a9fb9b2890b6af055f87f8e9f87514ee7fc32e
alma-mahmud <alma.mahmud.dev@gmail.com>
2026-02-26 23:12:04 +0600

    Hero section e image er pichone degin

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,11 +4,12 @@ import { GoVideo } from "react-icons/go";
 const Banner = () => {
   return (
     <>
-      <section className="relative bg-[#EFFFF9] py-10 md:py-20 min-h-screen flex items-center overflow-hidden">
-        <div className="container mx-auto py-10 lg:py-0 px-4">
-          <div className="flex flex-col md:flex-row gap-10 md:gap-20 lg:gap-35.25 items-center">
-            {/* Left Content Side */}
-            <div className="w-full lg:max-w-131.25 text-center md:text-left">
+      {/* min-h-screen and items-stretch full contain e h pabar jonno */}
+      <section className="relative bg-[#EFFFF9] min-h-screen flex items-stretch overflow-hidden">
+        <div className="container mx-auto px-4 relative z-20 flex">
+          <div className="flex flex-col md:flex-row gap-10 md:gap-20 lg:gap-35.25 items-center w-full">
+            {/* Left Content Side - text ke majhe rakhar jonno py-20  */}
+            <div className="w-full lg:max-w-131.25 text-center md:text-left py-20">
               <h1
                 className="text-[#1B1B1B] text-[36px] sm:text-[48px] md:text-[58px]
               font-normal leading-tight md:leading-[71.5px] font-lato"
@@ -29,19 +30,17 @@ const Banner = () => {
                 className="flex flex-col sm:flex-row items-center justify-center
               md:justify-start gap-4 md:gap-5"
               >
-                {/* Explore More Button */}
                 <button
                   className="w-full sm:w-auto bg-[#06C279] text-white
-                font-semibold rounded-[5px] py-3.5 px-8 hover:bg-[#05a868] transition-all"
+                font-semibold rounded-[5px] py-3.75 px-8.75 font-lato hover:bg-[#05a868] transition-all"
                 >
                   Explore More
                 </button>
 
-                {/* Watch Video Button */}
                 <button
                   className="w-full sm:w-auto flex items-center justify-center
                 gap-2 border-2 border-[#06C279] text-[#1B1B1B] font-semibold
-                rounded-[5px] py-3.5 px-8 hover:bg-[#06C279]/10 transition-all"
+                rounded-[5px] py-3.75 px-8.75 font-lato hover:bg-[#06C279]/10 transition-all"
                 >
                   <span className="text-[#06C279] text-[24px]">
                     <GoVideo />
@@ -51,20 +50,20 @@ const Banner = () => {
               </div>
             </div>
 
-            {/* Right Image Side */}
-            <div
-              className="relative mt-12 md:mt-0 w-full max-w-100
-            md:max-w-none"
-            >
+            {/* Right Image Side - image ke nice namanor jonno self-end and mt-auto */}
+            <div className="relative w-full flex-1 flex justify-center md:justify-end items-end self-end mt-auto">
+              {/* Background Circle - ডানে সরানোর জন্য left-[55%] ব্যবহার করা হয়েছে */}
               <div
-                className="absolute top-1/2 left-1/2 -translate-x-1/2
-              -translate-y-1/2 w-70 h-70 md:w-112.5 md:h-112.5 border-20
-              md:border-40 border-[#D1F7E9] rounded-full -z-10"
+                className="hidden lg:block absolute top-[55%] left-[55%] -translate-x-1/2 -translate-y-1/2 
+                w-70 h-70 md:w-112.5 md:h-112.5 border-20 md:border-40 
+                border-[#D1F7E9] rounded-full -z-10"
               ></div>
+
+              {/* Hero Image - বামে সরানোর জন্য md:-translate-x-10 ব্যবহার করতে পারেন */}
               <img
                 src={heroimg}
                 alt="Hero Image"
-                className="relative z-10 w-full  object-contain"
+                className="relative z-10 w-auto h-auto md:h-[85vh] max-h-screen object-contain block md:-translate-x-18"
               />
             </div>
           </div>
