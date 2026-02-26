77cb276129ea4fe3d17a69d2070934937066dc9a
alma-mahmud <alma.mahmud.dev@gmail.com>
2026-02-26 22:35:07 +0600

    Experience file add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navber from './components/Navber'
 import Banner from './components/Banner'
+import Experience from './components/Experience'
 
 const App = () => {
   return (
     <>
     <Navber></Navber>
     <Banner></Banner>
+    <Experience></Experience>
     </>
     
   )
