2f6709ea7dbaed92df597ab8c5bf08a584024b4a
alma-mahmud <alma.mahmud.dev@gmail.com>
2026-02-27 14:18:39 +0600

    Hero section padding fixed

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,12 +4,14 @@ import { GoVideo } from "react-icons/go";
 const Banner = () => {
   return (
     <>
-      {/* min-h-screen and items-stretch full contain e h pabar jonno */}
-      <section className="relative bg-[#EFFFF9] min-h-screen flex items-stretch overflow-hidden">
-        <div className="container mx-auto px-4 relative z-20 flex">
-          <div className="flex flex-col md:flex-row gap-10 md:gap-20 lg:gap-35.25 items-center w-full">
-            {/* Left Content Side - text ke majhe rakhar jonno py-20  */}
-            <div className="w-full lg:max-w-131.25 text-center md:text-left py-20">
+      {/* items-stretch এর বদলে items-center ব্যবহার করা হয়েছে যাতে বড় স্ক্রিনেও কন্টেন্ট মাঝে থাকে */}
+      <section className="relative bg-[#EFFFF9] flex items-center overflow-hidden">
+        <div className="container mx-auto px-4 relative z-20 flex md:pt-15 lg:py-14">
+          {/* md পর্যন্ত flex-col রাখা হয়েছে যাতে টেক্সটের নিচে ইমেজ থাকে, lg থেকে flex-row হবে */}
+          <div className="flex flex-col lg:flex-row gap-10 lg:gap-35.25 items-center w-full">
+            {/* Left Content Side */}
+            {/* py-10 দেওয়া হয়েছে যাতে মোবাইল ও ট্যাবলেটে (md) বিশাল প্যাডিং না থাকে, lg তে py-14 */}
+            <div className="w-full lg:max-w-131.25 text-center lg:text-left">
               <h1
                 className="text-[#1B1B1B] text-[36px] sm:text-[48px] md:text-[58px]
               font-normal leading-tight md:leading-[71.5px] font-lato"
@@ -20,7 +22,7 @@ const Banner = () => {
 
               <p
                 className="text-[#717171] text-[16px] md:text-[20px] font-normal
-              leading-normal font-lato mt-4 md:mt-6 mb-8 md:mb-10 max-w-137.5 mx-auto md:mx-0"
+              leading-normal font-lato mt-4 md:mt-6 mb-8 md:mb-10 max-w-137.5 mx-auto lg:mx-0"
               >
                 Boost Productivity and Wellness in Your Organization with
                 CoreWave's Advanced Tools and Techniques
@@ -28,7 +30,7 @@ const Banner = () => {
 
               <div
                 className="flex flex-col sm:flex-row items-center justify-center
-              md:justify-start gap-4 md:gap-5"
+              lg:justify-start gap-4 md:gap-5"
               >
                 <button
                   className="w-full sm:w-auto bg-[#06C279] text-white
@@ -50,20 +52,20 @@ const Banner = () => {
               </div>
             </div>
 
-            {/* Right Image Side - image ke nice namanor jonno self-end and mt-auto */}
-            <div className="relative w-full flex-1 flex justify-center md:justify-end items-end self-end mt-auto">
-              {/* Background Circle - left e soranor jonno left-[55%] */}
+            {/* Right Image Side */}
+            {/* Right Image Side */}
+            <div className="relative w-full flex-1 flex justify-center lg:justify-end items-end self-end mt-auto min-h-[300px] lg:min-h-[500px]">
+              {/* Background Circle */}
               <div
                 className="hidden lg:block absolute top-[55%] left-[55%] -translate-x-1/2 -translate-y-1/2 
-                w-70 h-70 md:w-112.5 md:h-112.5 border-20 md:border-40 
-                border-[#D1F7E9] rounded-full -z-10"
+    w-112.5 h-112.5 border-40 border-[#D1F7E9] rounded-full -z-10"
               ></div>
 
-              {/* Hero Image - Right e soranor jonno md:-translate-x-10  */}
+              {/* Hero Image - এখানে h-[75vh] এবং self-end নিশ্চিত করবে এটি নিচে লেগে থাকবে */}
               <img
                 src={heroimg}
                 alt="Hero Image"
-                className="relative z-10 w-auto h-auto md:h-[85vh] max-h-screen object-contain block md:-translate-x-18"
+                className="relative z-10 w-auto h-auto max-h-[50vh] md:max-h-[60vh] lg:max-h-[80vh] object-contain block lg:-translate-x-18 self-end"
               />
             </div>
           </div>
